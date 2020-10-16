323371c7a9a15f795e09ae39acadeb9edffce4d3
GaurishAnand <gaurishanand13@gmail.com>
2020-10-16 19:42:30 +0530

    youtube added

--- app/src/main/java/com/example/fliplearn/ui/main/HomeFragment.kt
+++ app/src/main/java/com/example/fliplearn/ui/main/HomeFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.HorizontalScrollView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fliplearn.R
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
@@ -17,6 +20,14 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view =  inflater.inflate(R.layout.fragment_home, container, false)
         //childFragmentManager.beginTransaction().replace(R.id.showYoutube, youtubeViewFragment()).commit()
+
+        /**
+         * Setting up the course Adapter
+         */
+        view.coursesRecyclerView.layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+        view.coursesRecyclerView.adapter = coursesAdapter(context!!)
+
+
         return view
     }
 }
